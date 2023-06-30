1c9ee20ac321078eff3af0a0979fc5f7377b619a
Lucas PICHOLLET <Lucas@MacBook-Pro-de-Lucas.local>
2023-06-30 12:16:16 +0200

    retouches carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -28,27 +28,39 @@ function Carousel({carouselPictures}) {
       };
     }, [showNum, carouselPictures.length]);
 
-    return (
+    if (carouselPictures.length <= 1) {
+      return (
         <div className="carousel-nav">
-            <div className="carousel-left">
-              <i className="fa-solid fa-chevron-left" onClick={() => carouselNext(true)}></i>
-            </div>
-  
-            <div className="carousel-content" style={{transform:`translateX(${-100*showNum}%)`}}>
-              {carouselPictures.map((picture, index) => (
-                <img src={picture} alt={index} className='carousel-content-item' key={index} />
-              ))}
-            </div>
-  
-            <div className="carousel-right">
-              <i className="fa-solid fa-chevron-right" onClick={() => carouselNext()}></i>
-            </div>
-
-            <div className="carousel-counter">
-              {currentPicture}/{carouselPictures.length}
-            </div>
+          <div className="carousel-content">
+            {carouselPictures.map((picture, index) => (
+              <img src={picture} alt={index} className="carousel-content-item" key={index} />
+            ))}
+          </div>
         </div>
-    )
+      );
+    }
+  
+    return (
+      <div className="carousel-nav">
+        <div className="carousel-left">
+          <i className="fa-solid fa-chevron-left" onClick={() => carouselNext(true)}></i>
+        </div>
+  
+        <div className="carousel-content" style={{ transform: `translateX(${-100 * showNum}%)` }}>
+          {carouselPictures.map((picture, index) => (
+            <img src={picture} alt={index} className="carousel-content-item" key={index} />
+          ))}
+        </div>
+  
+        <div className="carousel-right">
+          <i className="fa-solid fa-chevron-right" onClick={() => carouselNext()}></i>
+        </div>
+  
+        <div className="carousel-counter">
+          {currentPicture}/{carouselPictures.length}
+        </div>
+      </div>
+    );
 }
 
 export default Carousel
